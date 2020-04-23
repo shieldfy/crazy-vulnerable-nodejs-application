00669bcd4e42fc0224e8d493a7f4970b7272d57b
Eslam Salem <eslam@shieldfy.com>
2020-04-24 00:44:31 +0200

    try es6 syntax

--- vulnerabilities/xss.js
+++ vulnerabilities/xss.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/greeting', (req, res) => {res.send('<h1> Hello :'+ req.query.name +"</h1>")})
+router.get('/greeting', (req, res) => {
+    const { name }  = req.query;
+    res.send('<h1> Hello :'+ name +"</h1>")
+})
 
 module.exports = router
