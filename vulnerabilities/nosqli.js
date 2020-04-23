dec1f6f8b33fb271ad81cb2a153547facf37aa23
Eslam Salem <eslam@shieldfy.com>
2020-04-24 01:16:24 +0200

    add es6 async & await to nosql

--- vulnerabilities/nosqli.js
+++ vulnerabilities/nosqli.js
@@ -2,58 +2,72 @@ const express = require('express');
 const config = require('../config')
 const router = express.Router()
 
-
 const MongoClient = require('mongodb').MongoClient;
 const url = config.MONGODB_URI;
 
-router.get('/', (req, res) => res.send('Hello World!'))
-router.get('/register', (req, res) => res.send('Hello World!'))
+router.post('/customers/register', async (req, res) => {
 
-router.post('/customers/register', (req, res) => {
-  MongoClient.connect(url, function (err, db) {
-    if (err) throw err;
-    var dbo = db.db(config.MONGODB_DB_NAME);
-    var myobj = { name: req.body.name, address: req.body.address };
-    var customers = dbo.collection("customers")
+    const client = await MongoClient.connect(url, { useNewUrlParser: true })
+        .catch(err => { console.log(err); });
+    if (!client) {
+        return res.json({ status: "Error" });
+    }
+    const db = client.db(config.MONGODB_DB_NAME);
+    const customers = db.collection("customers")
+    
+    let myobj = { name: req.body.name, address: req.body.address };
     customers.insertOne(myobj, function (err) {
-      if (err) throw err;
-      console.log("user registered");
-      db.close();
+        if (err) throw err;
+        console.log("user registered");
+        res.json({ status:"success", "message": "user inserted" })
+        db.close();
     });
-    res.json("document inserted")
-  });
+    
 })
 
 
 // Vulnerable search function
-router.post('/customers/find', (req, res) => {
-  MongoClient.connect(url, function (err, db) {
-    if (err) throw err;
-    var dbo = db.db(config.MONGODB_DB_NAME);
-    var myobj = { name: req.body.name };
-    var customers = dbo.collection("customers")
+router.post('/customers/find', async (req, res) => {
+
+
+    const client = await MongoClient.connect(url, { useNewUrlParser: true })
+        .catch(err => { console.log(err); });
+    if (!client) {
+        return res.json({ status: "Error" });
+    }
+    const db = client.db(config.MONGODB_DB_NAME);
+    const customers = db.collection("customers")
+
+    let myobj = { name: req.body.name };
     customers.findOne(myobj, function (err, result) {
-      if (err) throw err;
-      db.close();
-      res.json(result)
+        if (err) throw err;
+        db.close();
+        res.json(result)
     });
-  });
+
+  
 })
 
 // Vulnerable Authentication
 // Authentication Bypass Example
 // curl -X POST http://localhost:3000/customers/login/ --data "{\"email\": {\"\$gt\":\"\"} , \"password\": {\"\$gt\":\"\"}}" -H "Content-Type: application/json"
 
-router.post('/customers/login', (req, res) => {
-  MongoClient.connect(url, function (err, db) {
-    if (err) throw err;
-    var dbo = db.db(config.MONGODB_DB_NAME);
-    var customers = dbo.collection("customers")
-    var myobj = { email: req.body.email, password: req.body.password };
+router.post('/customers/login', async (req, res) => {
+
+    const client = await MongoClient.connect(url, { useNewUrlParser: true })
+        .catch(err => { console.log(err); });
+    if (!client) {
+        return res.json({ status: "Error" });
+    }
+    const db = client.db(config.MONGODB_DB_NAME);
+    const customers = db.collection("customers")
+
+    let myobj = { email: req.body.email, password: req.body.password };
     customers.findOne(myobj, function (err, result) {
-      if (err) throw err;
-      db.close();
-      res.json(result)
+        if (err) throw err;
+        db.close();
+        res.json(result)
     });
-  });
+
+ 
 })
